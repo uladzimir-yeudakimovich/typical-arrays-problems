94452cb5bc6d4badb4564359b819e48396a498fd
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2021-02-10 02:13:24 +0300

    feat: min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  return array.sort((a, b) => a - b)[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  return array.sort((a, b) => a - b).reverse()[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  return array.reduce((a, b) => a + b, 0) / array.length;
 }
